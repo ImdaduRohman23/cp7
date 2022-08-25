e5bd0c561ddcdc7e3115f5f0441adb7a46cb4e8e
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-25 21:01:50 +0700

    belajaran setItem

--- src/assets/components/WebStorage/index.jsx
+++ src/assets/components/WebStorage/index.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 
 const WebStorage = () => {
     const data = ['SEMANGAT!','coba coba', 'ggfhghdgfh'];
+    // const dataGet = localStorage.getItem('data');
+
     return (
         <div style={{marginBottom: '50px'}}>
             <h3>Web Storage</h3>
             <button onClick={() => {
                 localStorage.setItem('data', JSON.stringify(data))
             }}>
-                Local Storage
+                Local Storage: Klik for <b>setItem</b> data
             </button>
+            {/* <button onClick={() => {
+                localStorage.removeItem('data')
+            }}>
+                Remove
+            </button>
+            <p>Ini dari <b>getItem</b>: {dataGet}</p> */}
+
         </div>
     )
 }
