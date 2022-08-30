bbf5e7770845c021faab14b10e6c835926244af2
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-08-30 08:03:50 +0700

    topik 2 finish

--- src/assets/components/WebStorage/index.jsx
+++ src/assets/components/WebStorage/index.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const WebStorage = () => {
     const data = ['SEMANGAT!','coba coba', 'ggfhghdgfh'];
-    // const dataGet = localStorage.getItem('data');
+    const dataGet = localStorage.getItem('data-user');
+
+    const [dataUser, setDataUser] = useState('');
+
+    const handleDataUser = () => {
+        setDataUser(localStorage.setItem('data-user', JSON.stringify(data)))
+    }
+
+    const handleRemoveData = () => {
+        localStorage.removeItem('data-user')
+    }
+
+    console.log(dataGet)
 
     return (
         <div style={{marginBottom: '50px'}}>
             <h3>Web Storage</h3>
-            <button onClick={() => {
-                localStorage.setItem('data', JSON.stringify(data))
-            }}>
-                Local Storage: Klik for <b>setItem</b> data
+
+            <button onClick={handleDataUser}>
+                Local Storage
             </button>
-            <button onClick={() => {
-                localStorage.removeItem('data')
-            }}>
+
+            <button onClick={handleRemoveData}>
                 Remove
             </button>
-            <p>Ini dari <b>getItem</b>: {localStorage.getItem('data')}</p>
+            
+            <p>Ini dari <b>getItem</b>: {dataGet}</p>
+
+            {/* {
+                dataGet ? <button onClick={handleRemoveData}>
+                Remove
+            </button>  : <button onClick={handleDataUser}>
+                Local Storage
+            </button>
+            } */}
 
         </div>
     )
